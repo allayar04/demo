e8060fc6640f0f8094e3109b952306d60dcb6d9a
Allayar Jandullaev <jandullaevallayar@gmail.com>
2024-08-16 19:46:54 +0500

    Done

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -1,9 +1,11 @@
 package com.task06;
 
+
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
@@ -14,13 +16,12 @@ import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.ISODateTimeFormat;
-
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.ISODateTimeFormat;
 
 @LambdaHandler(
     lambdaName = "audit_producer",
@@ -35,59 +36,62 @@ import java.util.UUID;
 @DynamoDbTriggerEventSource(targetTable = "Configuration", batchSize = 10)
 @DependsOn(name = "Configuration", resourceType = ResourceType.DYNAMODB_TABLE)
 public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
-	private final AmazonDynamoDB dynamoDBClient = AmazonDynamoDBClientBuilder.standard()
+	private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
 			.withRegion("eu-central-1")
 			.build();
-	private final String auditTableName = "cmtr-d0429c20-Audit-test";
+	private final String tableName = "cmtr-d2f4ab85-Audit-test";
 
 	@Override
 	public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
-		context.getLogger().log("Received DynamoDB Event: " + dynamodbEvent.getRecords());
+		context.getLogger().log("Event: " + dynamodbEvent.getRecords());
 
 		dynamodbEvent.getRecords().forEach(record -> {
-			Map<String, AttributeValue> auditRecord = generateAuditRecord(record, context);
-			context.getLogger().log("Generated audit record: " + auditRecord);
-			dynamoDBClient.putItem(new PutItemRequest().withTableName(auditTableName).withItem(auditRecord));
-			context.getLogger().log("Audit record saved to table: " + auditTableName);
+			context.getLogger().log("Record: " + record);
+
+			Map<String, AttributeValue> auditEntry = new HashMap<>();
+			String itemKey = record.getDynamodb().getKeys().get("key").getS();
+			auditEntry.put("id", new AttributeValue().withS(UUID.randomUUID().toString()));
+			auditEntry.put("itemKey", new AttributeValue().withS(itemKey));
+			auditEntry.put("modificationTime", new AttributeValue().withS(formatUsingJodaTime(org.joda.time.LocalDate.now())));
+
+			switch (record.getEventName()) {
+				case "INSERT":
+					createAuditEntry(auditEntry, record, context);
+					break;
+
+				case "MODIFY":
+					updateAuditEntry(auditEntry, record, context);
+					break;
+			}
 		});
 
 		return null;
 	}
 
-	private Map<String, AttributeValue> generateAuditRecord(DynamodbEvent.DynamodbStreamRecord record, Context context) {
-		Map<String, AttributeValue> auditEntry = new HashMap<>();
-		String configKey = record.getDynamodb().getKeys().get("key").getS();
-		auditEntry.put("id", new AttributeValue(UUID.randomUUID().toString()));
-		auditEntry.put("itemKey", new AttributeValue(configKey));
-		auditEntry.put("modificationTime", new AttributeValue(getCurrentTimestamp()));
+	private void createAuditEntry(Map<String, AttributeValue> auditEntry, DynamodbEvent.DynamodbStreamRecord record, Context context) {
+		context.getLogger().log("Creating audit entry for INSERT event.");
 
-		switch (record.getEventName()) {
-			case "INSERT":
-				processInsertEvent(record, auditEntry, configKey);
-				break;
-			case "MODIFY":
-				processModifyEvent(record, auditEntry);
-				break;
-		}
+		Map<String, AttributeValue> newValueMap = new HashMap<>();
+		newValueMap.put("key", new AttributeValue().withS(record.getDynamodb().getKeys().get("key").getS()));
+		newValueMap.put("value", new AttributeValue().withN(record.getDynamodb().getNewImage().get("value").getN()));
 
-		return auditEntry;
+		auditEntry.put("newValue", new AttributeValue().withM(newValueMap));
+
+		client.putItem(new PutItemRequest().withTableName(tableName).withItem(auditEntry));
 	}
 
-	private void processInsertEvent(DynamodbEvent.DynamodbStreamRecord record, Map<String, AttributeValue> auditEntry, String itemKey) {
-		Map<String, AttributeValue> newValues = new HashMap<>();
-		newValues.put("key", new AttributeValue(itemKey));
-		newValues.put("value", new AttributeValue(record.getDynamodb().getNewImage().get("value").getN()));
-		auditEntry.put("newValue", new AttributeValue().withM(newValues));
+	private void updateAuditEntry(Map<String, AttributeValue> auditEntry, DynamodbEvent.DynamodbStreamRecord record, Context context) {
+		context.getLogger().log("Updating audit entry for MODIFY event.");
+
+		auditEntry.put("updatedAttribute", new AttributeValue().withS("value"));
+		auditEntry.put("oldValue", new AttributeValue().withN(record.getDynamodb().getOldImage().get("value").getN()));
+		auditEntry.put("newValue", new AttributeValue().withN(record.getDynamodb().getNewImage().get("value").getN()));
+
+		client.putItem(new PutItemRequest().withTableName(tableName).withItem(auditEntry));
 	}
 
-	private void processModifyEvent(DynamodbEvent.DynamodbStreamRecord record, Map<String, AttributeValue> auditEntry) {
-		auditEntry.put("updatedAttribute", new AttributeValue("value"));
-		auditEntry.put("oldValue", new AttributeValue(record.getDynamodb().getOldImage().get("value").getN()));
-		auditEntry.put("newValue", new AttributeValue(record.getDynamodb().getNewImage().get("value").getN()));
-	}
-
-	private String getCurrentTimestamp() {
-		org.joda.time.LocalDate localDate = org.joda.time.LocalDate.now(DateTimeZone.UTC);
-		return ISODateTimeFormat.dateTime().print(localDate);
+	private String formatUsingJodaTime(org.joda.time.LocalDate localDate) {
+		org.joda.time.format.DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+		return formatter.print(localDate.toDateTimeAtStartOfDay(DateTimeZone.UTC));
 	}
 }
