09888ba23b11813abddeac86c288cfd2b4f35335
Allayar Jandullaev <jandullaevallayar@gmail.com>
2024-08-18 03:39:04 +0500

    Update

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -3,19 +3,15 @@ package com.task08;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.api.open_meteo.GetOpenMeteo;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "api_handler",
@@ -36,8 +32,6 @@ import java.util.Map;
 		invokeMode = InvokeMode.BUFFERED
 )
 public class ApiHandler implements RequestHandler<Object, String> {
-	private static final ObjectMapper mapper = new ObjectMapper();
-
 	@Override
 	public String handleRequest(Object o, Context context) {
 		try {
